1ef6e4fe0d117b66aeb9d70d241422758460a241
blackicedbear <lmikdy15@protonmail.com>
2022-06-26 17:38:38 +0200

    Added first files
    Added scripts for indexing posts and stories
    
    Name schema:
    posts/alina_22o8/alina_22o8-217836374_359879322166176_7492277944411971116_n-2021-07-14-16h-14m-25s.jpg
    posts/alina_22o8/alina_22o8-2021-07-14-16h-14m-25s.jpg
    stories/_strobl/_strobl-2021-09-05-18h-23m-39s.jpg
    stories/_strobl/_strobl-2021-09-05-18h-23m.jpg

---
+++ index_posts.py
@@ -0,0 +1,119 @@
+import os
+import pathlib
+import json
+import datetime
+from os.path import exists
+from tensorflow.keras.applications.xception import Xception
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.xception import preprocess_input
+from tensorflow.keras.applications.xception import decode_predictions
+import numpy as np
+
+model = Xception(
+    include_top=True,
+    weights="imagenet",
+    input_tensor=None,
+    input_shape=None,
+    pooling=None,
+    classes=1000,
+    classifier_activation="softmax",
+)
+
+WRITE_TO_FILE = True
+
+if __name__ == "__main__":
+    print("Lese JSON datei ein...")
+    if exists("posts_index.json"):
+        print("JSON Datei vorhanden! (posts_index.json)")
+        try:
+            f = open("posts_index.json", "r")
+            contents = f.read()
+            posts_index_file = json.loads(str(contents))
+            file_list_json = []
+            for x in posts_index_file:
+                file_list_json.append(x['filename'])
+            posts_index_file_exists = True
+            print("JSON Datei eingelesen.")
+        except:
+            posts_index_file = []
+            posts_index_file_exists = False
+            print("Fehler beim einlesen der Datei...")
+    else:
+        print("JSON Datei fehlt... Erstelle posts_index.json")
+        posts_index_file = []
+        posts_index_file_exists = False
+
+    print("Lese Dateiliste ein...")
+    file_list = []
+    profile_list = os.listdir('posts')
+    for x in profile_list:
+        try:
+            for y in os.listdir("posts/" + x):
+                if(pathlib.Path(y).suffix == ".jpg" or pathlib.Path(y).suffix == ".webp"):
+                    file_list.append(y)
+        except:
+            pass
+    print("Dateiliste eingelesen.")
+
+    if posts_index_file_exists == True:
+        try:
+            new_files = set(file_list) ^ set(file_list_json)
+        except:
+            pass
+    else:
+        new_files = file_list
+    for filename in new_files:
+        try:
+            split = filename.split("-")
+            if (len(split) == 8):
+                image1 = image.load_img("posts/" + split[0] + "/" + filename, target_size = (299, 299))
+                transformedImage = image.img_to_array(image1)
+                transformedImage = np.expand_dims(transformedImage, axis = 0)
+                transformedImage = preprocess_input(transformedImage)
+                prediction = model.predict(transformedImage)
+                predictionLabel = decode_predictions(prediction, top = 6)
+                predictionLabelNew = []
+                for x in predictionLabel[0]:
+                    predictionLabelNew.append(x[1])
+                time = datetime.datetime(int(split[2]), int(split[3]), int(split[4]), int(split[5].replace('h', '')), int(split[6].replace('m', '')), int(split[7].split('.')[0].replace('s', '')))
+                posts_index_file.append({
+                    "filename": filename,
+                    "profile": split[0],
+                    "id": split[1],
+                    "datetime": time.strftime("%d. %b %Y %H:%M:%S"),
+                    "fdatetime": str(time),
+                    "timestamp": time.timestamp(),
+                    "tags": predictionLabelNew
+                })
+            elif (len(split) == 7):
+                image1 = image.load_img("posts/" + split[0] + "/" + filename, target_size = (299, 299))
+                transformedImage = image.img_to_array(image1)
+                transformedImage = np.expand_dims(transformedImage, axis = 0)
+                transformedImage = preprocess_input(transformedImage)
+                prediction = model.predict(transformedImage)
+                predictionLabel = decode_predictions(prediction, top = 6)
+                predictionLabelNew = []
+                for x in predictionLabel[0]:
+                    predictionLabelNew.append(x[1])
+                time = datetime.datetime(int(split[1]), int(split[2]), int(split[3]), int(split[4].replace('h', '')), int(split[5].replace('m', '')), int(split[6].split('.')[0].replace('s', '')))
+                posts_index_file.append({
+                    "filename": filename,
+                    "profile": split[0],
+                    "datetime": time.strftime("%d. %b %Y %H:%M:%S"),
+                    "fdatetime": str(time),
+                    "timestamp": time.timestamp(),
+                    "tags": predictionLabelNew
+                })
+            else:
+                print(filename)
+                print(split)
+        except:
+            pass
+
+    if(WRITE_TO_FILE == True):
+        f = open("posts_index.json", "w")
+        print("Aktualisiere posts_index.json...")
+        f.write(str(posts_index_file).replace('(', '{').replace(')', '}').replace("'", '"'))
+        print("posts_index.json wurde aktualisiert.")
+        f.close()
---
+++ index_stories.py
@@ -0,0 +1,118 @@
+import os
+import pathlib
+import json
+import datetime
+from os.path import exists
+from tensorflow.keras.applications.xception import Xception
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.xception import preprocess_input
+from tensorflow.keras.applications.xception import decode_predictions
+import numpy as np
+
+model = Xception(
+    include_top=True,
+    weights="imagenet",
+    input_tensor=None,
+    input_shape=None,
+    pooling=None,
+    classes=1000,
+    classifier_activation="softmax",
+)
+
+WRITE_TO_FILE = True
+
+if __name__ == "__main__":
+    print("Lese JSON datei ein...")
+    if exists("stories_index.json"):
+        print("JSON Datei vorhanden! (stories_index.json)")
+        try:
+            f = open("stories_index.json", "r")
+            contents = f.read()
+            stories_index_file = json.loads(str(contents))
+            file_list_json = []
+            for x in stories_index_file:
+                file_list_json.append(x['filename'])
+            stories_index_file_exists = True
+            print("JSON Datei eingelesen.")
+        except:
+            stories_index_file = []
+            stories_index_file_exists = False
+            print("Fehler beim einlesen der Datei...")
+    else:
+        print("JSON Datei fehlt... Erstelle stories_index.json")
+        stories_index_file = []
+        stories_index_file_exists = False
+
+    print("Lese Dateiliste ein...")
+    file_list = []
+    profile_list = os.listdir('stories')
+    for x in profile_list:
+        try:
+            for y in os.listdir("stories/" + x):
+                if(pathlib.Path(y).suffix == ".jpg" or pathlib.Path(y).suffix == ".webp"):
+                    file_list.append(y)
+        except:
+            pass
+    print("Dateiliste eingelesen.")
+
+    if stories_index_file_exists == True:
+        try:
+            new_files = set(file_list) ^ set(file_list_json)
+        except:
+            pass
+    else:
+        new_files = file_list
+    for filename in new_files:
+        try:
+            split = filename.split("-")
+            if (len(split) == 7):
+                image1 = image.load_img("stories/" + split[0] + "/" + filename, target_size = (299, 299))
+                transformedImage = image.img_to_array(image1)
+                transformedImage = np.expand_dims(transformedImage, axis = 0)
+                transformedImage = preprocess_input(transformedImage)
+                prediction = model.predict(transformedImage)
+                predictionLabel = decode_predictions(prediction, top = 6)
+                predictionLabelNew = []
+                for x in predictionLabel[0]:
+                    predictionLabelNew.append(x[1])
+                time = datetime.datetime(int(split[1]), int(split[2]), int(split[3]), int(split[4].replace('h', '')), int(split[5].replace('m', '')), int(split[6].split('.')[0].replace('s', '')))
+                stories_index_file.append({
+                    "filename": filename,
+                    "profile": split[0],
+                    "datetime": time.strftime("%d. %b %Y %H:%M:%S"),
+                    "fdatetime": str(time),
+                    "timestamp": time.timestamp(),
+                    "tags": predictionLabelNew
+                })
+            elif (len(split) == 6):
+                image1 = image.load_img("stories/" + split[0] + "/" + filename, target_size = (299, 299))
+                transformedImage = image.img_to_array(image1)
+                transformedImage = np.expand_dims(transformedImage, axis = 0)
+                transformedImage = preprocess_input(transformedImage)
+                prediction = model.predict(transformedImage)
+                predictionLabel = decode_predictions(prediction, top = 6)
+                predictionLabelNew = []
+                for x in predictionLabel[0]:
+                    predictionLabelNew.append(x[1])
+                time = datetime.datetime(int(split[1]), int(split[2]), int(split[3]), int(split[4].replace('h', '')), int(split[5].replace('m', '')))
+                stories_index_file.append({
+                    "filename": filename,
+                    "profile": split[0],
+                    "datetime": time.strftime("%d. %b %Y %H:%M:%S"),
+                    "fdatetime": str(time),
+                    "timestamp": time.timestamp(),
+                    "tags": predictionLabelNew
+                })
+            else:
+                print(filename)
+                print(split)
+        except:
+            pass
+
+    if(WRITE_TO_FILE == True):
+        f = open("stories_index.json", "w")
+        print("Aktualisiere stories_index.json...")
+        f.write(str(stories_index_file).replace('(', '{').replace(')', '}').replace("'", '"'))
+        print("stories_index.json wurde aktualisiert.")
+        f.close()
